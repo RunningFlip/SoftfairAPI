e328d6370a84c4227ef172a11d92d3c287b4153f
RunningFlip <p.staymann@yahoo.com>
2024-11-06 19:11:42 +0100

    feat: adds Vergleichrechner and first implementation KostenNutzenVergleichsrechner

---
+++ SoftfairAPI/src/Vergleichsrechner/IVergleichsrechner.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftfairAPI; 
+
+/// <summary>
+/// Ein Interface erschien mir hier sinnig, damit verschiedene Vergleichsrechner mit der selben Methode
+/// <see cref="IVergleichsrechner{T}.VergleicheAngebote"/>> implementiert werden können.
+/// Möglicherweise inkludieren andere Vergleichsrechner weitere Logik oder können anderweitig angesteuert werden.
+/// </summary>
+/// <typeparam name="TAngebot">Angebots Typ der vom Vergleichsrechner verbearbeitet werden soll.</typeparam>
+public interface IVergleichsrechner<TAngebot> where TAngebot : Angebot {
+    TAngebot? VergleicheAngebote(List<TAngebot> angebote);
+}
---
+++ SoftfairAPI/src/Vergleichsrechner/KostenNutzenVergleichsrechner.cs
@@ -0,0 +1,19 @@
+﻿namespace SoftfairAPI; 
+
+/// <summary>
+/// Hier der gewünschte Vergleichsrechner der das beste Angebot auf Basis der monatlichen Prämie sowie der Deckungssumme
+/// ermittelt.
+/// </summary>
+public class KostenNutzenVergleichsrechner : IVergleichsrechner<Versicherungsangebot> {
+    
+    /// <summary>
+    /// Ich habe mich hier für eine einfache Variante entschieden, da noch nicht klar ist, wie viele Angebote es eigentlich geben wird.
+    /// Optimierung kommt im besten Falle immer zum Schluss, weshalb genau der Punkt hier ein Kandidat dafür wäre.
+    /// Bei einer sehr großen Liste, kann eine Alternative zum MinBy Aufruf attraktiv werden.
+    /// </summary>
+    /// <param name="angebote">Die Liste der übergebenen Angebote die vergleichen werden soll.</param>
+    /// <returns>Das beste Ergebnis im Vergleich der monatlichen Prämie sowie der Deckungssumm</returns>
+    public Versicherungsangebot? VergleicheAngebote(List<Versicherungsangebot> angebote) {
+        return angebote.MinBy(candidate => candidate.MonatlichePraemie / candidate.Deckungssumme);
+    }
+}
